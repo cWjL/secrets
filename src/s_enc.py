b4d37545e470bae326bace6ac42a6af8c3155a85
cWjL <jacobloden@hotmail.com>
2018-10-22 15:36:06 -0700

    working encoded strings

--- src/s_enc.py
+++ src/s_enc.py
@@ -1,4 +1,4 @@
-import binascii, base64
+import binascii, base64, sys
 
 class EncodedSec():
     def __init__(self, bin_path, chnk_sz):
@@ -7,50 +7,86 @@ class EncodedSec():
 
     def get_strs(self):
         '''
-        
+        Return list of printable strings
+
+        @param none
+        @return string list
         '''
         _printable_strs = []
         for cnk in self._read_bin_data(self.bin_path, self.chunk):
-            _byte_str = self._bin_to_hex_str(cnk)
-            if self._is_printable_char_str(_byte_str) is not None:
-                _printable_strs.append(self._is_printable_char_str(_byte_str))
+            try:
+                _printable_strs.append(base64.b64decode(cnk))
+            except Exception as e:
+                self._less(cnk[:(len(cnk)-4)], _printable_strs)
 
-    def _decode_nonprintable_strings(self, nonprintable_list):
+        tmp = self._get_printable_char_list(_printable_strs)
+        print("Length " + str(len(tmp)))
+        for i in tmp:
+            print(i)
+        sys.exit(0)
+
+        #return _printable_strs
+
+    def _less(self, data_cnk, lst):
         '''
-        Decode binary hex input
+        Check for base64 encoded strings by recursively shrinking the size of the binary 
+        data chunk by 4 bytes
 
-        @param list of binary data
-        @return list of decoded strings
+        @param binary data
+        @param string list
+        @return string list
+        '''
+        ### THIS IS BROKEN ###
+        ### RecursionError: maximum recursion depth exceeded in __instancecheck__
+        if len(data_cnk) == 0:
+            return
+        ####################################################################################
+        #tmp = data_cnk+b"\x3d\x3d\x3d"
+        #print(tmp)
+        #if b"\x3d\x3d\x3d" in tmp:
+        #    print("yep")
+        #sys.exit(0)
+        ####################################################################################
+        try:
+            lst.append(base64.b64decode(data_cnk))
+            self._less(data_cnk[:(len(data_cnk)-2)], lst)
+        except binascii.Error:
+            if b"\x3d\x3d\x3d" in data_cnk:
+                self._less(data_cnk[:(len(data_cnk)-6)], lst)
+            else:
+                self._less(data_cnk+b"\x3d", lst)
+            
+
+    def _decode(self, byte_in):
+        '''
+        Return hex string representation of binary string passed to it
+
+        @param binary bytes
+        @return decoded string
         '''
         _hex_str = ""
-        for i in nonprintable_list:
-            try:
-                _hex_str = i.replace('\\x', '')
-                _decoded.append(binascii.a2b_hex(_hex_str.replace(' ', '')))
-            except Exception as ex:
-                continue
+        try:
+            _hex_str = i.replace('\\x', '')
+            _decoded.append(binascii.a2b_hex(_hex_str.replace(' ', '')))
+        except Exception as ex:
+            return None
         return _decoded
 
-    def _is_printable_char_str(self, hex_arr):
+    def _get_printable_char_list(self, hex_list):
         '''
-        Determine if hex array contains printable string, return it if so
+        Determine if hex array contains printable strings, return a list 
+        of them if it so
 
         @param hex array
         @return string or none
         '''
-        _result = ""
-        try:
-            _decode = base64.b64decode(hex_arr, validate=True)
-        except binascii.Error:
-            return None
-        retrun _result
-        # for i in _decode:
-            # if i in string.printable and "\n" not in i  and "\t" not in i:
-                # _result += i
-                # continue # next loop iteration, skipping the next if statement
-            # if len(_result) >= self.min_len:
-                # yield _result
-            # _result = ""
+        _result = []
+        for i in hex_list:
+            try:
+                _result.append(binascii.unhexlify(i))
+            except:
+                continue
+        return _result
 
     def _bin_to_hex_str(self, encoded_data):
         '''
@@ -80,8 +116,6 @@ class EncodedSec():
             while True:
                 chunk = f.read(data_size)
                 if chunk:
-                    #for b in chunk:
-                    #    yield b
-                    yield from chunk
+                    yield chunk
                 else:
                     break
