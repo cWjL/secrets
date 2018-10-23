8849930b47f1b7ec7653f209a5d239a654609fd1
cWjL <jacobloden@hotmail.com>
2018-10-22 21:21:21 -0700

    added secret string

--- test/test_c_with_base64_string.c
+++ test/test_c_with_base64_string.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 // secrets.py test binary source
+char *get_super_duper(char*);
 
 int main(void){
   char buff[5];
   char *secret_no_pad = "eW91ciBtb20K"; //"your mom"
   char *secret_with_pad = "eW91ciBtb20gCg=="; //"your mom "
   char *super_secret = "MTg2Mzk5MzA1NWQ3ZGJlYzkxMGZmODAwYzViODA5ZmMgIC0K"; // "your mom" | md5sum | base64
+  //echo "secret string..shh" | base64 >> c2VjcmV0IHN0cmluZy4uc2hoCg==  with '==' removed
+  char super_duper_secret[29] = "c2VjcmV0IHN0cmluZy4uc2hoCg";
+
   printf("Hello! Enter 'p' for padded, 'n' for no pad, or 's' for super secret: \n");
   fgets(buff,4,stdin);
   
@@ -21,6 +26,12 @@ int main(void){
   }else{
     printf("you're a fucking moron aren't you?\n");
   }
+  printf(get_super_duper(super_duper_secret));
+  printf("\n");
   return 0;
+}
+
+char *get_super_duper(char *str){
+  return strcat(str, "==");
 }
   
