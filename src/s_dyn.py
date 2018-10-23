06d61504574769bc9a8a1c2700e3041e9ff786fb
cWjL <jacobloden@hotmail.com>
2018-10-23 14:54:16 -0700

    added angr import

--- src/s_dyn.py
+++ src/s_dyn.py
@@ -1,1 +1,7 @@
-#TODO implement this
+import angr
+
+class DynamicSec():
+    def __init__(self, bin_path):
+        self.proj = angr.Project(bin_path)
+        self.entry = self.proj.entry
+        self.state = self.proj.factory.entry_state()
