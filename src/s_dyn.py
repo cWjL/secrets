a75e028d6ad5c2415563a4d342f881dd98c69c47
cWjL <jacobloden@hotmail.com>
2018-12-13 15:06:13 -0700

    tmp dyn functionality

--- src/s_dyn.py
+++ src/s_dyn.py
@@ -1,7 +1,7 @@
-import angr
+#import angr
+import floss, vivisect
 
 class DynamicSec():
     def __init__(self, bin_path):
-        self.proj = angr.Project(bin_path)
-        self.entry = self.proj.entry
-        self.state = self.proj.factory.entry_state()
+        self.vw = vivisect.VivWorkspace()
+        
